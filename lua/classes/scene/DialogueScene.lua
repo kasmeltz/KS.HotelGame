cf8d5766f8ceff93706e191741528601884997e7
kasmeltz <kasmeltz@lakeheadu.ca>
2016-05-11 09:54:34 -0500

    wip

--- lua/classes/scene/DialogueScene.lua
+++ lua/classes/scene/DialogueScene.lua
@@ -12,7 +12,8 @@ function DialogueScene:draw()
 	local sh = self.screenHeight
 	local dialogue = self.dialogue
 	local branch = dialogue.currentBranch
-	local option = branch[dialogue.selectedOption]
+	
+	local option = branch.options[dialogue.selectedOption]
 	
 	love.graphics.setColor(255, 255, 255, 128)
 	love.graphics.rectangle('fill', 50, 50, sw - 100, sh - 100)
@@ -22,7 +23,8 @@ function DialogueScene:draw()
 	love.graphics.setColor(128,64,64)
 	love.graphics.rectangle('fill', 75, 525, sw - 150, 300)
 	
-	love.graphics.printf(option.text, 75, 550)
+	love.graphics.setColor(255,255,255)
+	love.graphics.printf(option.text, 75, 550, sw - 150)
 end
 
 function DialogueScene:keyreleased(key, scancode)
