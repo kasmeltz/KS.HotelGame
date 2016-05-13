b64e3f54cce3c291091947bc2a7776a8febab2be
kasmeltz <kasmeltz@lakeheadu.ca>
2016-05-12 20:27:08 -0500

    Another bug squashed! Need to calculate middle of triangle without taking position of object into consideration. Duh! Rendering is looking good!

--- lua/classes/scene/ShearTestScene.lua
+++ lua/classes/scene/ShearTestScene.lua
@@ -9,6 +9,18 @@ local cpml = require 'libs/cpml'
 local roofMesh
 local wallMesh
 
+function ShearTestScene:findMiddle(mesh)
+	for _, triangle in ipairs(mesh) do	
+		local mx, my, mz = 0, 0, 0
+		for _, vertex in ipairs(triangle.vertices) do
+			mx = mx + vertex[1]
+			my = my + vertex[2]
+			mz = mz + vertex[3]
+		end
+		triangle.middle = { mx / 3, my / 3, mz / 3 }
+	end
+end
+
 function ShearTestScene:init()
 	ShearTestScene.super.init(self)
 	self.roofImage = love.graphics.newImage('data/images/roof.jpg')
@@ -38,6 +50,8 @@ function ShearTestScene:init()
 			normal = { 0, 0, -1 }
 		}
 	}	
+	
+	self:findMiddle(roofMesh)	
 	
 	wallMesh = 
 	{
@@ -115,25 +129,31 @@ function ShearTestScene:init()
 			normal = { 0, -1, 0 }
 		}
 	}
+	
+	self:findMiddle(wallMesh)
 end
 
-local cam = {0, 0, -10}
+local cam = {0, 0, -7}
 
 local building1Pos = {0, 0, 0}
+local building2Pos = {6, 0, 0}
+local building3Pos = {0, 6, 0}
+local building4Pos = {6, 6, 0}
+local building5Pos = {-10, 0, 0}
 
 function ShearTestScene:addMeshToRender(mesh, pos)
 	local meshesToRender = self.meshesToRender
 	
 	-- move verts according to object and camera
-	local movedMesh = { texture = mesh.texture }
+	local movedMesh = { texture = mesh.texture, position = pos }
 	for _, triangle in ipairs(mesh) do
-		local movedTriangle = { normal = triangle.normal, vertices = {} }
+		local movedTriangle = { normal = triangle.normal, middle = triangle.middle,  vertices = {} }
 		for _, vertex in ipairs(triangle.vertices) do	
 			local movedVertex = 
 			{			
-				vertex[1] + cam[1] - pos[1], 
-				vertex[2] + cam[2] - pos[2], 
-				vertex[3] + cam[3] - pos[3], 
+				vertex[1] + cam[1] + pos[1], 
+				vertex[2] + cam[2] + pos[2], 
+				vertex[3] + cam[3] + pos[3], 
 				vertex[4], 
 				vertex[5] 
 			}
@@ -151,26 +171,18 @@ function ShearTestScene:renderMeshes()
 	-- sort triangles
 	local orderedTriangles = {}	
 	for _, mesh in ipairs(meshesToRender) do
+		local pos = mesh.position
 		for _, triangle in ipairs(mesh) do	
 			local n = triangle.normal
-			local mx, my, mz, lx, ly, lz = 0, 0, 0, 0, 0, 0
-			for _, vertex in ipairs(triangle.vertices) do
-				mx = mx + vertex[1]
-				my = my + vertex[2]
-				mz = mz + vertex[3]
-			end
-			mx = mx / 3
-			my = my / 3
-			mz = mz / 3
+			local middle = triangle.middle
+			local mx = middle[1] + pos[1]
+			local my = middle[2] + pos[2]
+			local mz = middle[3] + pos[3]
 			local lx = mx + cam[1]
 			local ly = my + cam[2]
 			local lz = mz + cam[3]			
 			local dot = n[1] * lx + n[2] * ly + n[3] * lz		
-
-			--print('dot')			
-			--print(dot)
-			
-			if dot > 1 then
+			if dot > 0 then
 				local dx = mx - cam[1]
 				local dy = my - cam[2]
 				local dz = mz - cam[3]
@@ -215,22 +227,30 @@ function ShearTestScene:draw()
 	
 	self:addMeshToRender(wallMesh, building1Pos)
 	self:addMeshToRender(roofMesh, building1Pos)	
+	self:addMeshToRender(wallMesh, building2Pos)
+	self:addMeshToRender(roofMesh, building2Pos)	
+	self:addMeshToRender(wallMesh, building3Pos)
+	self:addMeshToRender(roofMesh, building3Pos)	
+	self:addMeshToRender(wallMesh, building4Pos)
+	self:addMeshToRender(roofMesh, building4Pos)	
+	self:addMeshToRender(wallMesh, building5Pos)
+	self:addMeshToRender(roofMesh, building5Pos)	
 
 	self:renderMeshes()	
 end
 
 function ShearTestScene:update(dt)	
 	if love.keyboard.isDown('a') then
-		cam[1] = cam[1] + dt * 2
-	end
-	if love.keyboard.isDown('d') then
 		cam[1] = cam[1] - dt * 2
 	end
+	if love.keyboard.isDown('d') then
+		cam[1] = cam[1] + dt * 2
+	end
 	if love.keyboard.isDown('w') then
-		cam[2] = cam[2] - dt * 2
+		cam[2] = cam[2] + dt * 2
 	end
 	if love.keyboard.isDown('s') then
-		cam[2] = cam[2] + dt * 2
+		cam[2] = cam[2] - dt * 2
 	end
 	if love.keyboard.isDown('q') then
 		cam[3] = cam[3] - dt * 2
